9cc129f2e4c3749c31212bf829e144c268834d09
Nasser Mohamed <nassermohamedit@gmail.com>
2024-02-19 22:17:45 +0100

    added equals method

--- src/main/java/mohamednasser/projects/http/HttpRequest.java
+++ src/main/java/mohamednasser/projects/http/HttpRequest.java
@@ -56,4 +56,18 @@ public class HttpRequest {
     void setMethod(String methodName) {
         setMethod(HttpMethod.valueOf(methodName));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HttpRequest)) return false;
+        HttpRequest r = (HttpRequest) o;
+        if (!this.method.equals(r.getMethod())) return false;
+        if (!this.target.equals(r.getTarget())) return false;
+        if (!this.version.equals(r.getVersion())) return false;
+        Map<String, String> headers = new HashMap<>();
+        r.getHeaders().forEach(e -> headers.put(e.getKey(), e.getValue()));
+        if (!this.headers.equals(headers)) return false;
+        return true;
+    }
 }
